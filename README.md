fd8214ffdadd60ea4c4d7aa69138dd5b63e0636b
tedr56 <tedr56@gmail.com>
2013-11-22 04:59:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NodeBB SoundCLoud Plugin
+
+This NodeBB plugin allows users to embed SoundCloud tracks inline in their posts.
+
+## Installation
+
+    npm install nodebb-plugin-vimeo
---
+++ library.js
@@ -0,0 +1,15 @@
+(function(module) {
+	"use strict";
+
+	var SoundCloud = {},
+		embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/$1/$2"></iframe>';
+
+
+	Vimeo.parse = function(postContent, callback) {
+		// modified from http://stackoverflow.com/questions/7168987/
+		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?(.+)\/?(.+)<\/a>/g, embed);
+		callback(null, postContent);
+	};
+
+	module.exports = SoundCloud;
+}(module));
