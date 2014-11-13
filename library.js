afd403a31aec87feb5d0078cb292fb7f8babe89a
Aaron Smith <a5mith@users.noreply.github.com>
2014-11-13 12:37:44 +0000

    Update for 0.6.0
    Move Regular Expressions out of post.parse. Support for 0.6.0

--- library.js
+++ library.js
@@ -3,19 +3,21 @@
     var Soundcloud = {},
         embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/$2&amp;show_artwork=true"></iframe>',
         embeds = '<iframe width="100%" height="410" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/sets/$2&amp;show_artwork=true"></iframe>'
+    var	embedtrack = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
+    var	embedset = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/sets\/([\w\-_]+)">.+<\/a>/g;
 
-    Soundcloud.parse = function(postContent, callback) {
-        var	embedtrack = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
-        var	embedset = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/sets\/([\w\-_]+)">.+<\/a>/g;
-
-        if (postContent.match(embedtrack)) {
-            postContent = postContent.replace(embedtrack, embed);
+    Soundcloud.parse = function(data, callback) {
+        if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
         }
-        if (postContent.match(embedset)) {
-            postContent = postContent.replace(embedset, embeds);
+        if (data.postData.content.match(embedtrack)) {
+            data.postData.content = data.postData.content.replace(embedtrack, embed);
+        }
+        if (data.postData.content.match(embedset)) {
+            data.postData.content = data.postData.content.replace(embedset, embeds);
         }
 
-        callback(null, postContent);
+        callback(null, data);
     };
 
     module.exports = Soundcloud;
