dc95635fc00d1a9189e4a8952b7644ae5651cf24
Adrien Rocher Vj <a.rocher.vj@gmail.com>
2015-09-16 14:31:49 +0200

    NodeBB 0.7 0.8 compatibility

--- library.js
+++ library.js
@@ -1,12 +1,14 @@
 (function(module) {
     "use strict";
     var Soundcloud = {},
+
         embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/$2&amp;show_artwork=true"></iframe>',
         embeds = '<iframe width="100%" height="410" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/sets/$2&amp;show_artwork=true"></iframe>'
-    var	embedtrack = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
-    var	embedset = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/sets\/([\w\-_]+)">.+<\/a>/g;
 
-    Soundcloud.parse = function(data, callback) {
+    var	embedtrack = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/([\w\-_]+)" (?:rel="nofollow")?>.+<\/a>/g;
+    var	embedset = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/sets\/([\w\-_]+)" (?:rel="nofollow")?>.+<\/a>/g;
+    
+    Soundcloud.parsePost = function(data, callback) {
         if (!data || !data.postData || !data.postData.content) {
             return callback(null, data);
         }
